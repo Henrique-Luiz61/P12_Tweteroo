53c76227a35e98766d9330ef800add7051bb01f7
Henrique <henriqueluiz266@gmail.com>
2023-06-24 17:46:21 -0300

    versão sem bônus

--- src/app.js
+++ src/app.js
@@ -6,13 +6,10 @@ app.use(cors());
 app.use(express.json());
 
 const arrUsers = [];
-const userNames = [];
 const arrTweets = [];
-const objTweet = {
-  username: "",
-  avatar: "",
-  tweet: "",
-};
+const userNames = [];
+
+//https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png
 
 app.post("/sign-up", (req, res) => {
   const newUser = {
@@ -21,7 +18,6 @@ app.post("/sign-up", (req, res) => {
   };
   arrUsers.push(newUser);
   userNames.push(newUser.username);
-  objTweet.avatar = newUser.avatar;
 
   res.send("OK");
 });
@@ -33,24 +29,20 @@ app.post("/tweets", (req, res) => {
 
   const newTweet = {
     username: req.body.username,
+    avatar: arrUsers[arrUsers.length - 1].avatar,
     tweet: req.body.tweet,
   };
+
+  if (arrTweets.length === 10) {
+    let tweetRemoved = arrTweets.shift();
+  }
+
   arrTweets.push(newTweet);
-  objTweet.username = newTweet.username;
-  objTweet.tweet = newTweet.tweet;
 
   res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  if (arrTweets.length === 10) {
-    arrTweets.shift();
-  }
-
-  if (objTweet.tweet !== "") {
-    arrTweets.push(objTweet);
-  }
-
   res.send(arrTweets);
 });
 
